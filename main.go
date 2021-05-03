ae127c2fd84c6c862ffc3e59f86418df1fbf8b5f
Justis <justis710@gmail.com>
2021-05-03 11:20:49 -0400

    initial for go server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprint(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+
+	http.HandleFunc("/", echoString)
+
+	http.HandleFunc("/increment", incrementCounter)
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "HI")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+// this is super freaking easy
